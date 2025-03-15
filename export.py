9930d48dcc112e31f75e9684051338acee808a19
孙达明 <shun_@outlook.com>
2025-03-15 20:08:09 +0800

    feat:更新导出

--- export.py
+++ export.py
@@ -1,4 +1,5 @@
+from ultralytics import YOLO
 # 模型转换
-# model = YOLO("best.pt")
-# model.export(format='onnx') # 导出 ONNX 模型
-# model.export(format='engine') # 导出 TensorRT 模型
+model = YOLO("best.pt")
+model.export(format='onnx') # 导出 ONNX 模型
+model.export(format='engine') # 导出 TensorRT 模型
