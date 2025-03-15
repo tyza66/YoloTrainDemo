a3b2e66810a3335bbac0ad07940b7d6dc25baad8
孙达明 <shun_@outlook.com>
2025-03-15 19:57:41 +0800

    docs:添加注释

--- export.py
+++ export.py
@@ -1,4 +1,4 @@
 # 模型转换
 # model = YOLO("best.pt")
-# model.export(format='onnx')
-# model.export(format='engine')
+# model.export(format='onnx') # 导出 ONNX 模型
+# model.export(format='engine') # 导出 TensorRT 模型
