a548b517d4bf7c106515860bba7bca48760edef9
孙达明 <shun_@outlook.com>
2025-03-15 17:18:09 +0800

    feat:初始化训练集和项目

---
+++ export.py
@@ -0,0 +1,4 @@
+# 模型转换
+# model = YOLO("best.pt")
+# model.export(format='onnx')
+# model.export(format='engine')
---
+++ train.py
@@ -0,0 +1,10 @@
+# 模型训练
+from ultralytics import YOLO
+import os
+os.environ['NO_ALBUMENTATIONS_UPDATE'] = '1'
+
+if __name__ == '__main__':
+    # 1. 初始化模型 这个不一定好用 但是小 演示快
+    model = YOLO("yolo11n.pt")
+    # 2. 模型训练 数据集配置文件 训练轮数 输入图像大小
+    model.train(data='train_config.yaml', epochs=100,imgsz=640)
